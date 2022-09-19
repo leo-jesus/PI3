f9bbd3e3a31bb7dcf1c3ac2b59cee4eb952db03a
leo-jesus <llxdgb1@gmail.com>
2022-09-19 20:23:47 -0300

    server created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
+const server = require("http").createServer((request, response) => {
+  response.writeHead(204, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+  });
+  response.end("end");
+});
 
+const socketIo = require("socket.io");
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    credentials: false,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  socket.on("join-room", (roomId, userId) => {
+    //adiciona usuário
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnected!", roomId, userId);
+    socket.on(roomId).broadcast.emit("user-disconnected", userId);
+  });
+});
+
+const startServer = () => {
+  const { address, port } = server.address();
+  console.info(`app running at ${address}:${port}`);
+};
+
+server.listen(process.env.PORT || 3000, startServer);
